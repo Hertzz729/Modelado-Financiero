4e8813386182038a99e0ee5f16141a4af8767a06
Hertzz729 <101437898+Hertzz729@users.noreply.github.com>
2026-07-31 20:35:16 -0600

    Update imports relativos

--- modelado_financiero/volatilidad/volatilidad.py
+++ modelado_financiero/volatilidad/volatilidad.py
@@ -37,8 +37,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from typing import Sequence, TypeAlias
 
-from opciones.precios import Black_Scholes
-from opciones.griegas import Vega
+from .precios import Black_Scholes
+from .griegas import Vega
 
 ArregloComo: TypeAlias = Sequence[float]
 MatrizComo: TypeAlias = Sequence[Sequence[float]]

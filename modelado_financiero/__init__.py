5970b5bf98020f6f207a54712d9aae8ae642be2d
Hertzz729 <101437898+Hertzz729@users.noreply.github.com>
2026-07-31 19:41:03 -0600

    Update __init__.py

--- modelado_financiero/__init__.py
+++ modelado_financiero/__init__.py
@@ -9,6 +9,26 @@ from .opciones import (
     OpcionForex
 )
 
+from .opciones.griegas import (
+    Delta,
+    Gamma,
+    Theta,
+    Vega,
+    Rho,
+
+    Delta_A_div,
+    Gamma_A_div,
+    Theta_A_div,
+    Vega_A_div,
+    Rho_A_div,
+
+    Delta_B76,
+    Gamma_B76,
+    Theta_B76,
+    Vega_B76,
+    Rho_B76,
+
+)
 from .riesgo import (
     VaR_parametrico,
     VaR_historico,
